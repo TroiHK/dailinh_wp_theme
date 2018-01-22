f2a5dbb25a1a69b5a2a5f6f35a0f408a241d1d12
uyen.dang <uyen.dang@weareathena.com>
2018-01-23 01:13:17 +0700

    single

--- template-parts/single/gallery-video.php
+++ template-parts/single/gallery-video.php
@@ -7,14 +7,6 @@ $galleryArray = array();
         <div class="center">
             <div class="entry-gallery__imgs">
                 <?php foreach ($gallery_items as $item) : ?>
-                <?php  
-                    $myObj = (object) array();
-                    $myObj->type = "Hình ảnh";
-                    $myObj->url = $item['url'];
-                    $myObj->title = $item['title'];
-                    $myObj->legend = $item['caption'];
-                    $galleryArray[] = $myObj;
-                ?>
                     <article class="entry-gallery__img js-reveal"> 
                         <figure>
                             <img src="<?=$item['url']?>" alt="<?=$item['title']?>">
@@ -22,37 +14,6 @@ $galleryArray = array();
                         <figcaption><?=$item['caption']?></figcaption>
                     </article>
                 <?php endforeach; ?>
-
-                <?php if( have_rows('video') ): while( have_rows('video') ): the_row(); 
-                    // $item = get_sub_field('ing_post_url_video');
-                    $myObj = (object) array();
-
-                    $myObj->type = "video";
-                    $myObj->url = get_sub_field('ing_post_url_video');
-                    $myObj->title = get_sub_field('title');
-                    $myObj->legend = get_sub_field('caption');
-
-                    $galleryArray[] = $myObj;
-                endwhile;endif; ?>
-                <script>
-                    var galleryArray = <?=json_encode($galleryArray);?>;
-                </script>
-                <?php if(get_field('ing_post_bg_video')):?>
-                    <?php $image_ =  get_field('ing_post_bg_video'); ?>
-                    <article class="entry-gallery__img js-reveal">
-                        <h2>
-                            <small class="word-breaker js-reveal"><?=__('Hình ảnh & video','virtua-swpt')?></small>
-                            <span class="word-breaker js-reveal"><?=__('Thư viện','virtua-swpt')?></span>
-                        </h2>
-
-                        <a href="#" class="trigger-gallery js-reveal" data-gallery="galleryArray" data-trigger-hook="1">
-                            <figure>
-                                <img src="<?=$image_['url']?>" alt="<?=$image_['title']?>">
-                            </figure>
-                            <i><span></span></i>
-                        </a>
-                    </article>
-                <?php endif; ?>
             </div>
         </div>
     </section>
