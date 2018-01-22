2059da10f7e1c9435d6e3df8c2070b3d0a1be314
uyen.dang <uyen.dang@weareathena.com>
2018-01-23 02:16:06 +0700

    single

--- template-parts/blocks/content-main_menu.php
+++ template-parts/blocks/content-main_menu.php
@@ -26,18 +26,7 @@
 			<div class="grid">
 				<div class="grid__row">
 					<div class="grid__col-xxs--12">
-						<?php //if ( have_rows('footer_link_opt', 'option') ) : ?>
-							<?php //while ( have_rows('footer_link_opt', 'option') ) : the_row(); ?>
-								<!-- <a href="<?php the_sub_field('link'); ?>" class="footer-link"><?php the_sub_field('text'); ?></a> -->
-							<?php //endwhile; ?>
-						<?php //endif; ?>
 						<?php wp_nav_menu( array('theme_location' => 'footer_menu' ,'container' => false, 'menu_class' => 'footer-link')); ?>
-
-						<?php 
-						if(get_field('languages_showhide','option') == '1'){
-							echo my_custom_switcher_footer(); 
-						}
-						?>
 					</div>
 				</div>
 			</div>
