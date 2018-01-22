9ff1a8371021e76a557ad7a32bfc9c8ddef6585f
uyen.dang <uyen.dang@weareathena.com>
2018-01-23 00:33:29 +0700

    single

--- template-parts/single/gallery-video.php
+++ template-parts/single/gallery-video.php
@@ -24,13 +24,13 @@ $galleryArray = array();
                 <?php endforeach; ?>
 
                 <?php if( have_rows('video') ): while( have_rows('video') ): the_row(); 
-                    $item = get_sub_field('ing_post_url_video');
+                    // $item = get_sub_field('ing_post_url_video');
                     $myObj = (object) array();
 
                     $myObj->type = "video";
-                    $myObj->url = $item['url'];
-                    $myObj->title = $item['title'];
-                    $myObj->legend = $item['caption'];
+                    $myObj->url = get_sub_field('ing_post_url_video');
+                    $myObj->title = get_sub_field('title');
+                    $myObj->legend = get_sub_field('caption');
 
                     $galleryArray[] = $myObj;
                 endwhile;endif; ?>
@@ -41,8 +41,8 @@ $galleryArray = array();
                     <?php $image_ =  get_field('ing_post_bg_video'); ?>
                     <article class="entry-gallery__img js-reveal">
                         <h2>
-                            <small class="word-breaker js-reveal"><?=__('Photos & vidéos','virtua-swpt')?></small>
-                            <span class="word-breaker js-reveal"><?=__('Galerie','virtua-swpt')?></span>
+                            <small class="word-breaker js-reveal"><?=__('Hình ảnh & video','virtua-swpt')?></small>
+                            <span class="word-breaker js-reveal"><?=__('Thư viện','virtua-swpt')?></span>
                         </h2>
 
                         <a href="#" class="trigger-gallery js-reveal" data-gallery="galleryArray" data-trigger-hook="1">
