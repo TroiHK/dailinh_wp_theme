bae9da73b7c777fedd5893c80b5d72013a3e8428
uyen.dang <uyen.dang@weareathena.com>
2018-01-28 23:59:08 +0700

    update 404 image

--- search-form.php
+++ search-form.php
@@ -1,6 +1,6 @@
 <div class="block-search">
 	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="text" name="s" placeholder="<?=__('Recherche','utweb-dailinh')?>" id="search-key">
+		<input type="text" name="s" placeholder="<?=__('Tìm kiếm','utweb-dailinh')?>" id="search-key">
 		<button type="submit" style="width: 50px;text-align: right;cursor: pointer;">
 			<?php echo getSvgIcon('arrow-right', '0 0 11.4 11.4'); ?>
 		</button>
