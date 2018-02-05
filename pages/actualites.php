70a63e3fd8e256a4962f833e9268da93572874b3
troi.hoang <troi.hoang@weareathena.com>
2018-02-05 09:06:09 +0700

    last update

--- pages/actualites.php
+++ pages/actualites.php
@@ -7,7 +7,6 @@ $modulejs = 'default';
 get_header();
 $post_type = get_field('utweb_post_type');
 $taxanomy = get_field('utweb_taxanomy');
-var_dump($taxanomy);
 ?>
 
 <section class="section">
@@ -38,6 +37,8 @@ var_dump($taxanomy);
                                 'terms'    => $taxanomy,
                             );
                         }
+
+                        var_dump($args);
 
                         $posts = new WP_Query( $args );
 
